e36d51493665308172d2adcbfb6a306320311ad8
sml <sml@MacBook-Pro-7.local>
2022-03-29 21:30:17 -0300

    feat/preload: base of animations

--- src/components/SecondSection/VideosAnimation/VideosAnimation.jsx
+++ src/components/SecondSection/VideosAnimation/VideosAnimation.jsx
@@ -10,38 +10,110 @@ import { ScrollTrigger } from "gsap/all";
 
 export function VideosAnimation({ someText }) {
   const videoOneRef = useRef(null);
+  const divVideoOneRef = useRef(null);
+  const videoTwoRef = useRef(null);
+  const divVideoTwoRef = useRef(null);
+  const videoThreeRef = useRef(null);
+  const divVideoThreeRef = useRef(null);
   const videosRef = useRef(null);
 
   gsap.registerPlugin(ScrollTrigger);
 
-  // useEffect(() => {
-  //   gsap.from(videoOneRef.current, { 
-  //     scrollTrigger:  {
-  //       trigger: videosRef.current,
-  //       toggleActions: "restart pause reverse pause",
-  //       start: "start center",
-  //       scrub: true,
-  //       markers: true
-  //     },
-  //     x: "-54vw",
-  //     rotation: 40,
-  //   });
-  // }, []);
+  useEffect(() => {
+    gsap.from(videoOneRef.current, { 
+      scrollTrigger:  {
+        trigger: videosRef.current,
+        toggleActions: "restart none reverse none",
+        start: "top bottom",
+        end: "top-1px bottom",
+      },
+      x: "-54vw",
+      rotation: 40,
+      opacity: 0,
+    }); 
+
+    gsap.to(divVideoOneRef.current, { 
+      scrollTrigger:  {
+        trigger: videosRef.current,
+        start: "top center",
+        end: "top+=20% center",
+        scrub: true,
+      },
+      y: "-34vw",
+      rotation: 50,
+    }); 
+
+    gsap.from(videoTwoRef.current, { 
+      scrollTrigger:  {
+        trigger: videoTwoRef.current,
+        toggleActions: "restart none reverse none",
+        start: "top bottom",
+        end: "top-1px bottom",
+      },
+      x: "-54vw",
+      rotation: 30,
+      opacity: 0,
+    }); 
+
+    gsap.to(divVideoTwoRef.current, { 
+      scrollTrigger:  {
+        trigger: divVideoThreeRef.current,
+        start: "top center",
+        end: "top+=80% center",
+        markers: true,
+        scrub: true,
+      },
+      y: "-34vw",
+      x:"-13vw",
+      rotation: -30,
+    }); 
+
+    gsap.from(videoThreeRef.current, { 
+      scrollTrigger:  {
+        trigger: videoThreeRef.current,
+        toggleActions: "restart none reverse none",
+        start: "top bottom",
+        end: "top-1px bottom",
+      },
+      x: "54vw",
+      rotation: 70,
+      opacity: 0,
+    }); 
+
+    gsap.to(divVideoThreeRef.current, { 
+      scrollTrigger:  {
+        trigger: divVideoThreeRef.current,
+        start: "top center",
+        end: "top+=80% center",
+        markers: true,
+        scrub: true,
+      },
+      y: "-14vw",
+      rotation: 70,
+    }); 
+  }, []);
 
   return(
   <div ref={videosRef} className="videos-animations-container">
-      <video ref={videoOneRef} className="video-animations-one" preload="metadata" muted autoPlay loop> 
-        <source src={video} type="video/mp4"/>
-      </video>
+      <div ref={divVideoOneRef}>
+        <video ref={videoOneRef} className="video-animations-one" preload="metadata" muted autoPlay loop> 
+          <source src={video} type="video/mp4"/>
+        </video>
+      </div>
+
       <img src={Moon} className="moon-second-section"/>
       <p className="middle-text">Para viver mais e melhor <br/> são necessários cuidados preventivos<br/> com o nosso corpo </p>
       <div className="bottom-videos-container">
-        <video className="video-animations-two" preload="metadata" muted autoPlay loop>
-          <source src={video} type="video/mp4"/>
-        </video>
-        <video className="video-animations-three" preload="metadata" muted autoPlay loop>
-          <source src={video} type="video/mp4"/>
-        </video>
+        <div ref={divVideoTwoRef} className="video-animations-two">
+          <video ref={videoTwoRef} preload="metadata" muted autoPlay loop>
+            <source src={video} type="video/mp4"/>
+          </video>
+        </div>
+        <div ref={divVideoThreeRef} className="video-animations-three" >
+          <video ref={videoThreeRef} preload="metadata" muted autoPlay loop>
+            <source src={video} type="video/mp4"/>
+          </video>
+        </div>
       </div>
       <p className="bottom-text">e tudo começa de dentro para fora</p>
       <div className="b12-orbit-container">
