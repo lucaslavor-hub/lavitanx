dfb6fe41618c42c67fdff116ed680502626bf0f0
Sofia Melo <sofianovica@hotmail.com>
2022-05-04 20:39:57 -0300

    fix css and html structure

--- src/components/Footer/Form/index.jsx
+++ src/components/Footer/Form/index.jsx
@@ -2,57 +2,44 @@ import "./styles.css";
 
 export function Form (){
 
-    return(
+  return(
+    <div style={{textAlign: 'center'}}>
+      <form method="POST"  action="https://ecommercecimed.activehosted.com/proc.php" target="_blank" id="_form7" className="_form _form_7 _inline-form _inline-style _dark" noValidate>
+        <input type="hidden" name="u" defaultValue={7} />
+        <input type="hidden" name="f" defaultValue={7} />
+        <input type="hidden" name="s" />
+        <input type="hidden" name="c" defaultValue={0} />
+        <input type="hidden" name="m" defaultValue={0} />
+        <input type="hidden" name="act" defaultValue="sub" />
+        <input type="hidden" name="v" defaultValue={2} />
+        <input type="hidden" name="or" defaultValue="694f86de68067c9d1f85bd67652152da" />
+        <div className="_form-content">
+          <div className="_form_element _x64191235 _inline-style ">
 
-<div style={{textAlign: 'center'}}>
-  <form method="POST"  action="https://ecommercecimed.activehosted.com/proc.php" target="_blank" id="_form7" className="_form _form_7 _inline-form _inline-style _dark" noValidate>
-    <input type="hidden" name="u" defaultValue={7} />
-    <input type="hidden" name="f" defaultValue={7} />
-    <input type="hidden" name="s" />
-    <input type="hidden" name="c" defaultValue={0} />
-    <input type="hidden" name="m" defaultValue={0} />
-    <input type="hidden" name="act" defaultValue="sub" />
-    <input type="hidden" name="v" defaultValue={2} />
-    <input type="hidden" name="or" defaultValue="694f86de68067c9d1f85bd67652152da" />
-    <div className="_form-content">
-      <div className="_form_element _x64191235 _inline-style ">
+            <div className="_field-wrapper">
+              <input type="text" id="fullname" name="fullname" placeholder="Nome" required />
+            </div>
+          </div>
+          <div className="_form_element _x50559507 _inline-style ">
 
-        <div className="_field-wrapper">
-          <input type="text" id="fullname" name="fullname" placeholder="Nome" required />
+            <div className="_field-wrapper">
+              <input type="text" id="email" name="email" placeholder="Email" required />
+            </div>
+        
+          </div>
+          <div className="_button-wrapper _inline-style">
+            <button id="_form_7_submit" className="_submit" type="submit">
+            Inscreva-se
+            </button>
+          </div>
+          <div className="_clear-element">
+          </div>
         </div>
-      </div>
-      <div className="_form_element _x50559507 _inline-style ">
-
-        <div className="_field-wrapper">
-          <input type="text" id="email" name="email" placeholder="Email" required />
+        <div className="_form-thank-you" style={{display: 'none'}}>
         </div>
-    
-      </div>
-      <div className="_button-wrapper _inline-style">
-        <button id="_form_7_submit" className="_submit" type="submit">
-        Inscreva-se
-        </button>
-      </div>
-      <div className="_clear-element">
-      </div>
+      </form>
     </div>
-    <div className="_form-thank-you" style={{display: 'none'}}>
-    </div>
-  </form>
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-        )
+  )
 
 };
 
--- src/components/cSection/index.jsx
+++ src/components/cSection/index.jsx
@@ -5,7 +5,7 @@ import logo22 from "../../assets/logo_22.png"
 import Popup from '../Popup/index';
 import video from "../../assets/video.mp4";
 
-export function Csection (){
+export function Csection () {
 
     const [isOpen, setIsOpen] = useState(false);
   
@@ -13,37 +13,33 @@ export function Csection (){
       setIsOpen(!isOpen);}
 
     return(
-
-
-<div className="row2">
-            <div className="col2 col-2" >
-                  <img className="imagem" src={caixa} alt="Logo"/>
-                  </div>
-                  <div className="col2 col-2" id="coluna1">
-                  <div className="wrapper">
-                    <div><img className="imagem22" src={logo22} alt="Logo"/></div>
-                    <div className="s11"><a href="https://www.comprecimed.com.br/products/suplemento-alimentar-lavitan-bio-complex-com-30-comprimidos"  target="_blank"><button class="button button1">Comprar</button></a><a href="https://flynowspaceclub.com.br/"  target="_blank"><button class="button button2">FlyNowSpaceClub</button></a></div>
-                    <div><button onClick={togglePopup} class="button button3">Vídeo Informativo</button></div>
-              </div>
-              <div>
-
-    {isOpen && <Popup
-      content={<>
-<iframe id="video_popup2" width="715" height="auto"
-src="https://www.youtube.com/embed/563aZcYpg0s">
-</iframe>
-<iframe id="video_popup" width="700" height="auto"
-src="https://www.youtube.com/embed/563aZcYpg0s">
-</iframe>
-      </>}
-      handleClose={togglePopup}
-    />}
-  </div>
+      <div className="row2">
+        <div className="col2 col-2" >
+            <img className="imagem" src={caixa} alt="Logo"/>
+          </div>
+          <div className="col2 col-2" id="coluna1">
+            <div className="wrapper">
+              <div><img className="imagem22" src={logo22} alt="Logo"/></div>
+              <div className="s11"><a href="https://www.comprecimed.com.br/products/suplemento-alimentar-lavitan-bio-complex-com-30-comprimidos"  target="_blank"><button class="button button1">Comprar</button></a><a href="https://flynowspaceclub.com.br/"  target="_blank"><button class="button button2">FlyNowSpaceClub</button></a></div>
+              <div><button onClick={togglePopup} class="button button3">Vídeo Informativo</button></div>
             </div>
-            </div>
-
-        )
-
+          <div>
+            {isOpen && <Popup
+              content={<>
+                <iframe id="video_popup2" width="715" height="auto"
+                src="https://www.youtube.com/embed/563aZcYpg0s">
+                </iframe>
+                <iframe id="video_popup" width="700" height="auto"
+                src="https://www.youtube.com/embed/563aZcYpg0s">
+                </iframe>
+              </>}
+              handleClose={togglePopup}
+            />}
+          </div>
+        </div>
+      </div>
+      )
   };
-    export default Csection;
+
+export default Csection;
 
