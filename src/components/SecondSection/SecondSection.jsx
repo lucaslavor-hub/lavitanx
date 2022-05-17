a152a7b7ea202b17f06f7d5908420333cd6bc02c
Sofia Melo <sofianovica@hotmail.com>
2022-05-17 00:31:25 -0300

    change-animation

--- src/components/SecondSection/SecondSection.jsx
+++ src/components/SecondSection/SecondSection.jsx
@@ -49,27 +49,8 @@ export function SecondSection({ someText }) {
       duration: 1,
       ease: Power4.easeOut,
     }); 
-
-    // timelineGsap(".s1", 2)
-    // timelineGsap(".s2", 5)
-    // timelineGsap(".s3", 3)
-    // timelineGsap(".s4", 3.5)
-    // timelineGsap(".s5", 9.5)
-    // timelineGsap(".s6", 2.5)
-    // timelineGsap(".s7", 4)
-    // timelineGsap(".s8", 6)
-    // timelineGsap(".s9", 7)
-    // timelineGsap(".s10", 8)
-    // timelineGsap(".sec11", 9)
-
   }, []);
 
-  function timelineGsap(target, delay){
-    var tl = gsap.timeline({repeat: -1, repeatDelay: 0.1});
-    tl.to(target, {opacity: 1, duration: 0.1, delay:delay})
-    tl.to(target, {opacity: 0.3, duration: 0.1, ease:Power0.easeOut});
-    tl.to(target, {opacity: 1, duration: 0.3})
-  }
   
   return(
   <div ref={backgroundRef} className="background-second-section">
@@ -86,45 +67,8 @@ export function SecondSection({ someText }) {
       <div className="third-pill-container">
         <AnimatedPill width="100%" imgSrc={pill9} />
       </div>
-      {/* <h2 className="longevity-title text-animation">
-        <span className="s1">L</span>
-        <span className="s2">O</span>
-        <span className="s3">N</span>
-        <span className="s4">G</span>
-        <span className="s5">E</span>
-        <span className="s6">V</span>
-        <span className="s7">I</span>
-        <span className="s8">D</span>
-        <span className="s9">A</span>
-        <span className="s10">D</span>
-        <span className="s11">E</span>
-      </h2> */}
-       <h2 className="longevity-title text-animation">
-        <span className="letter1">L</span>
-        <span className="letter2">O</span>
-        <span className="letter3">N</span>
-        <span className="letter4">G</span>
-        <span className="letter5">E</span>
-        <span className="letter6">V</span>
-        <span className="letter7">I</span>
-        <span className="letter8">D</span>
-        <span className="letter9">A</span>
-        <span className="letter10">D</span>
-        <span className="letter11">E</span>
-      </h2>
-      {/* <div className="longevity-title text-animation">
-        <img className="s1 longevity-letter" src={L}/>
-        <img className="s2 longevity-letter" src={O}/>
-        <img className="s3 longevity-letter" src={N}/>
-        <img className="s4 longevity-letter" src={G}/>
-        <img className="s5 longevity-letter" src={E}/>
-        <img className="s6 longevity-letter" src={V}/>
-        <img className="s7 longevity-letter" src={I}/>
-        <img className="s8 longevity-letter" src={D}/>
-        <img className="s9 longevity-letter" src={A}/>
-        <img className="s10 longevity-letter" src={D2}/>
-        <img className="sec11 longevity-letter" src={E2}/>
-      </div> */}
+
+       <h2 className="longevity-title text-animation"> LONGEVIDADE </h2>
       <p className="longevity-subtitle text-animation">CUIDAR DO FUTURO <br></br> COMEÇA <p className="longevity-subtitle-bold">AGORA</p></p>
       <img className="shadow-right" src={shadowRight}></img>
       <img className="shadow-left" src={shadowLeft}></img>
